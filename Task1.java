95c8c80ebc3f9e112390d0ac9761c65e17616a72
Donthula-Vyshnavi <157142740+Donthula-Vyshnavi@users.noreply.github.com>
2024-01-29 22:06:25 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,11 @@
+Prompt the user to enter a text or provide a file to count the words.
+ Read the input text or file and store it in a string.
+ Split the string into an array of words using space or punctuation as delimiters.
+Initialize a counter variable to keep track of the number of words.
+Iterate through the array of words and increment the counter for each word encountered.
+ Display the total count of words to the user.
+You can further enhance the project by adding features such as:
+ Ignoring common words or stop words.
+ Providing statistics like the number of unique words or the frequency of each word.
+ Implementing input validation to handle empty inputs or file errors.
+Adding a graphical user interface (GUI) for a more user-friendly experience
